ca52ed1eaee6beed1ed867d4e786359a786c42d6
alice_piva <alice.piva@boraso.com>
2018-09-11 17:07:04 +0200

    add configuration for show checkbox invoice request

--- app/code/community/Webgriffe/InvoiceRequest/Helper/Data.php
+++ app/code/community/Webgriffe/InvoiceRequest/Helper/Data.php
@@ -7,4 +7,10 @@
  */
 class Webgriffe_InvoiceRequest_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    CONST ENABLE_INVOICE_REQUEST = 'customer/invoicerequest/enable_invoice_request';
+
+    public function getEnableInvoiceRequest()
+    {
+        return Mage::getStoreConfig(self::ENABLE_INVOICE_REQUEST);
+    }
 }
